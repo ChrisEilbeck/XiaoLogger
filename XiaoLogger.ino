bda3fed9a5412b857fe5f3c5fed118262c7a7782
Chris Eilbeck <chris@hyperspace.org.uk>
2022-03-15 14:13:54 +0000

    Intiial commit

---
+++ XiaoLogger.ino
@@ -0,0 +1,33 @@
+
+/*
+	Pin Allocations
+	===============
+	
+	0	-	Spare
+	
+	1	-	Spare
+	
+	2	-	Spare
+	
+	3	-	CS to SD breakout
+	
+	4	-	SDA to sensors
+	
+	5	-	SCL to sensors
+	
+	6	-	TX to GPS
+	
+	7	-	RX from GPS
+	
+	8	-	SCK to SD breakout
+	
+	9	-	MISO to SD breakout
+	
+	10	-	MOSI to SD breakout
+	
+	3v3	-	Power to SD breakout with its own regulator removed
+	
+	GND	-	General
+	
+	5v	-	Power to sensors with own regulators and GPS
+*/
