4b4b5d71ea6d300e6dba62e7efe41d6bbd1ef830
Chris Eilbeck <chris@hyperspace.org.uk>
2022-04-22 00:21:14 +0100

    This is now including the OLED display which is fed with GPS data

--- XiaoLogger.ino
+++ XiaoLogger.ino
@@ -307,88 +307,12 @@ void PollPressureSensor(uint32_t now)
 	}	
 }
 
-#include <TinyGPS.h>
+#include <TinyGPS++.h>
 
-TinyGPS gps;
+TinyGPSPlus gps;
 
 char linebuffer[80];
 int lineptr=0;
-
-
-
-static void print_float(float val, float invalid, int len, int prec);
-static void print_int(unsigned long val, unsigned long invalid, int len);
-static void print_date(TinyGPS &gps);
-static void print_str(const char *str, int len);
-
-
-
-
-static void print_float(float val, float invalid, int len, int prec)
-{
-  if (val == invalid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-}
-
-static void print_int(unsigned long val, unsigned long invalid, int len)
-{
-  char sz[32];
-  if (val == invalid)
-    strcpy(sz, "*******");
-  else
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-}
-
-static void print_date(TinyGPS &gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    Serial.print(sz);
-  }
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-}
-
-static void print_str(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-}
-
-
-
-
-
-
-
 
 
 
@@ -401,14 +325,6 @@ void SetupGps(void)
 {
 	Serial1.begin(9600);
 	
-  Serial.print("Testing TinyGPS library v. "); Serial.println(TinyGPS::library_version());
-  Serial.println("by Mikal Hart");
-  Serial.println();
-  Serial.println("Sats HDOP Latitude  Longitude  Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum");
-  Serial.println("          (deg)     (deg)      Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail");
-  Serial.println("-------------------------------------------------------------------------------------------------------------------------------------");
-
-
 
 
 
@@ -450,55 +366,6 @@ void PollGps(uint32_t now)
 		{
 			if(strstr(linebuffer,"GPGLL")!=NULL)
 			{
-#if 0
-				Serial.print("NumSats = ");
-				Serial.print(gps.satellites());
-				Serial.println();
-				
-				Serial.print("HDOP = ");
-				Serial.print(gps.hdop());
-				Serial.println();
-				
-				float flat;
-				float flon;
-				unsigned long age;
-				
-				gps.f_get_position(&flat, &flon, &age);
-				
-				Serial.print("Latitude = ");
-				Serial.print(flat);
-				Serial.println();
-				
-				Serial.print("Longitude = ");
-				Serial.print(flon);
-				Serial.println();
-#else
-				float flat, flon;
-				unsigned long age, date, time, chars = 0;
-				unsigned short sentences = 0, failed = 0;
-				static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
-				
-				print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
-				print_int(gps.hdop(), TinyGPS::GPS_INVALID_HDOP, 5);
-				gps.f_get_position(&flat, &flon, &age);
-				print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
-				print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
-				print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-				print_date(gps);
-				print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
-				print_float(gps.f_course(), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-				print_float(gps.f_speed_kmph(), TinyGPS::GPS_INVALID_F_SPEED, 6, 2);
-				print_str(gps.f_course() == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(gps.f_course()), 6);
-				print_int(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0xFFFFFFFF : (unsigned long)TinyGPS::distance_between(flat, flon, LONDON_LAT, LONDON_LON) / 1000, 0xFFFFFFFF, 9);
-				print_float(flat == TinyGPS::GPS_INVALID_F_ANGLE ? TinyGPS::GPS_INVALID_F_ANGLE : TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-				print_str(flat == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON)), 6);
-				
-				gps.stats(&chars, &sentences, &failed);
-				print_int(chars, 0xFFFFFFFF, 6);
-				print_int(sentences, 0xFFFFFFFF, 10);
-				print_int(failed, 0xFFFFFFFF, 9);
-				Serial.println();
-#endif
 			}
 			
 			lineptr=0;
@@ -551,10 +418,30 @@ void PollOLEDDisplay(uint32_t now)
 	
 	if(now>(last_poll+poll_timer))
 	{
+		display.clearDisplay();
+
+		display.setTextSize(1);             // Normal 1:1 pixel scale
+		display.setTextColor(SSD1306_WHITE);        // Draw white text
+
+		float flat;
+		float flon;
+		unsigned long age;
+				
+		display.setCursor(0,0);             // Start at top-left corner
+
+		display.print("Lat: ");
+		display.println(gps.location.lat(),6);
+
+		display.print("Lon: ");
+		display.println(gps.location.lng(),6);		
 		
+		display.print("Alt: ");
+		display.println(gps.altitude.meters());
+
+		display.print("Numsats: ");
+		display.println(gps.satellites.value());
 		
-		
-		
+		display.display();
 		
 		last_poll=now;
 	}
@@ -577,12 +464,14 @@ void setup(void)
 {
 	Serial.begin(115200);
 	
+#if 0
 	while ( !Serial ) delay(100);	 // wait for native usb
+#endif
 	
 //	SetupSDCard();
 //	SetupBeeper();
 //	SetupButtons();
-//	SetupOLEDDisplay();
+	SetupOLEDDisplay();
 	SetupAccelerometer();
 	SetupPressureSensor();
 	SetupGps();
@@ -595,7 +484,7 @@ void loop(void)
 //	PollSDCard(now);
 //	PollBeeper(now);
 //	PollButtons(now);
-//	PollOLEDDisplay(now);
+	PollOLEDDisplay(now);
 	PollAccelerometer(now);
 	PollPressureSensor(now);
 	PollGps(now);
